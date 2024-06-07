17ed9e31c1244f968f911a8a370481e761e56cfa
Bruno Castagnino Rossi <87082300+BCastagninoRossi@users.noreply.github.com>
2024-06-07 16:48:57 -0300

    algo

--- file.c
+++ file.c
@@ -9,7 +9,22 @@
  * TODO
  */
 int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum, void *buf) {
+    struct inode in;
+    if (inode_iget(fs, inumber, &in) < 0) {
+        return -1;
+    }
+    int size = inode_getsize(&in);
     
-    return 0;
+    int num_blocks = size / DISKIMG_SECTOR_SIZE;
+    int aux = num_blocks % DISKIMG_SECTOR_SIZE;
+    if (aux != 0) {
+        num_blocks++;
+    }
+    if (blockNum < num_blocks-1){
+        return 512;
+    }
+    if (blockNum > num_blocks-1){
+        return 0;
+    }
+    return size % DISKIMG_SECTOR_SIZE;
 }
-
