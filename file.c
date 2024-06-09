79581fd34d3b57eba9d5f0b145ae6aa11dbfbd99
Bruno Castagnino Rossi <87082300+BCastagninoRossi@users.noreply.github.com>
2024-06-09 11:37:58 -0300

    hola

--- file.c
+++ file.c
@@ -15,14 +15,14 @@ int file_getblock(struct unixfilesystem *fs, int inumber, int blockNum, void *bu
     }
     int64_t size = inode_getsize(&in);
 
-    // int16_t real_block_num = inode_indexlookup(fs, &in, blockNum);
-    // if (real_block_num < 0){
-    //     return -1;   
-    // }
+    int16_t real_block_num = inode_indexlookup(fs, &in, blockNum);
+    if (real_block_num < 0){
+        return -1;   
+    }
     
-    // if (diskimg_readsector(fs->dfd, real_block_num, buf) <0) {
-    //     return -1;
-    // }
+    if (diskimg_readsector(fs->dfd, real_block_num, buf) <0) {
+        return -1;
+    }
 
     int64_t num_blocks = size / DISKIMG_SECTOR_SIZE;
     int64_t aux = num_blocks % DISKIMG_SECTOR_SIZE;
