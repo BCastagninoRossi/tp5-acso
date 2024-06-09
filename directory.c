a4d8d65d763e9bab6b3d0b4d44da83254926e0fe
Bruno Castagnino Rossi <87082300+BCastagninoRossi@users.noreply.github.com>
2024-06-09 18:02:05 -0300

    eojrn

--- directory.c
+++ directory.c
@@ -15,21 +15,29 @@ int directory_findname(struct unixfilesystem *fs, const char *name, int dirinumb
   if (inode_iget(fs, dirinumber, &in) < 0) {
     return -1;
   }
+
+	int is_dir = ((in.i_mode & IFMT) == IFDIR);
+	if(!is_dir) return -1;
+
   int size = inode_getsize(&in);
-  if (size == 0) {
+  if (size <= 0) {
     return -1;
   }
+
   int num_blocks = size / DISKIMG_SECTOR_SIZE;
   int aux = num_blocks % DISKIMG_SECTOR_SIZE;
   if (aux != 0) {
     num_blocks++;
   }
+
   for (int i = 0; i < num_blocks; i++) {
-    if (file_getblock(fs, dirinumber, i, dirEnt) < 0) {
-      return -1;
-    }
-    for (int j = 0; j < DISKIMG_SECTOR_SIZE / sizeof(struct direntv6); j++) {
-      if (strcmp(dirEnt[j].d_name, name) == 0) {
+    struct direntv6 dirent_array[DISKIMG_SECTOR_SIZE / sizeof(struct direntv6)];
+    int valid_bytes = file_getblock(fs, dirinumber, i, dirent_array);
+    if(valid_bytes < 0) return -1;
+    int num_entries_in_block = valid_bytes / sizeof(struct direntv6);
+    for (int j = 0; j < num_entries_in_block; j++) {
+      if (strcmp(dirent_array[j].d_name, name) == 0) {
+        *dirEnt = dirent_array[j];
         return 0;
       }
     }
