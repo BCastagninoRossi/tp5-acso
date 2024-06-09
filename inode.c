eedb55e737f68042dfc5c73e201cae8216679cc1
Bruno Castagnino Rossi <87082300+BCastagninoRossi@users.noreply.github.com>
2024-06-08 21:55:20 -0300

    mes

--- inode.c
+++ inode.c
@@ -32,58 +32,47 @@ int inode_iget(struct unixfilesystem *fs, int inumber, struct inode *inp) {
  * TODO
  */
 int inode_indexlookup(struct unixfilesystem *fs, struct inode *inp, int blockNum) {  
+    //Implement code here
     int inode_file_size = inode_getsize(inp);
     if (inode_file_size == 0) return -1;
-
-    int inode_blocks = (inode_file_size + DISKIMG_SECTOR_SIZE - 1) / DISKIMG_SECTOR_SIZE;
+    int inode_blocks = inode_file_size/DISKIMG_SECTOR_SIZE;
     if (blockNum < 0 || blockNum >= inode_blocks) return -1;
-
     if (inode_file_size <= DISKIMG_SECTOR_SIZE*8){
         return inp->i_addr[blockNum];
     }
     int indir_block_num = blockNum / 256;
     int offset = blockNum % 256;
-
     int *indir_block = malloc(DISKIMG_SECTOR_SIZE);
-    if (indir_block == NULL) {
-        return -1;
-    }
-
     if (indir_block_num < 7){
         if (diskimg_readsector(fs->dfd, inp->i_addr[indir_block_num], indir_block) == -1) {
         free(indir_block);
         return -1;
-        } 
-        int block = indir_block[offset];
-        free(indir_block);
-        return block;
-    } else {  
-            int double_indir_block_num = (blockNum - 7*256) / 256;
-            int double_offset = (blockNum - 7*256) % 256;
-
-            if (diskimg_readsector(fs->dfd, inp->i_addr[7], indir_block) == -1) {
-                free(indir_block);
-            return -1;
-            } 
-
+        } else {
+            int block = indir_block[offset];
+            free(indir_block);
+            return block;
+        }
+    }
+    if (indir_block_num >= 7){
+        int indir_block_num = (blockNum - 7*256) / 256;
+        int offset = (blockNum - 7*256) % 256;
+        if (diskimg_readsector(fs->dfd, inp->i_addr[7], indir_block) == -1) {
+            free(indir_block);
+        return -1;
+        } else {
             int *second_indir_block = malloc(DISKIMG_SECTOR_SIZE);
-            if (second_indir_block == NULL) {
-                free(indir_block);
-                return -1;
-            }
-
-            if (diskimg_readsector(fs->dfd, indir_block[double_indir_block_num], second_indir_block) == -1) {
+            if (diskimg_readsector(fs->dfd, indir_block[indir_block_num], second_indir_block) == -1) {
                 free(second_indir_block);
                 free(indir_block);
                 return -1;
             }
-
-            int block = second_indir_block[double_offset];
+            int block = second_indir_block[offset];
             free(indir_block);
             free(second_indir_block);
             return block;
-        }
-    return -1;
+            }
+    }
+    return 0;
 }
 
 int inode_getsize(struct inode *inp) {
